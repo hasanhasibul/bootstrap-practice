51a40c342d448d6c069dcd70d64c46f830c50a73
Hasibul Hasan <hasanhasibul395@gmail.com>
2022-03-11 00:33:55 +0600

    boostrap practice

---
+++ login.js
@@ -0,0 +1,8 @@
+document.getElementById('RegistrationButton').addEventListener('click',function(){
+    document.getElementById('register').style.display="block"
+    document.getElementById('login').style.display="none"
+})
+document.getElementById('loginButton').addEventListener('click',function(){
+    document.getElementById('login').style.display="block"
+    document.getElementById('register').style.display="none"
+})
